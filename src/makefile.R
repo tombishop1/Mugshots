4c11c3777fa9c74a84ec48862d1da740261132fb
Tom Bishop <tombishopemail@gmail.com>
2021-09-13 12:09:20 +0100

    Update makefile.R

--- src/makefile.R
+++ src/makefile.R
@@ -5,12 +5,16 @@ library(dplyr)
 # setup plot area
 pdf(file = "output/staff.pdf", 
     height = 16.5,
-    width = 11.7)
+    width = 11.7
+   )
 
-# plot
+# set plot layout options
 par(mfrow = c(6, 4), 
     mar = c(1,1,1,1),
-    oma = c(1,1,3,1))
+    oma = c(1,1,3,1)
+   )
+
+# populate with images, labels
 for(i in list.files("data", full.names = TRUE)){
   plot(load.image(i), 
        axes = FALSE, 
@@ -21,6 +25,7 @@ for(i in list.files("data", full.names = TRUE)){
       )
 }
 
+# write title
 mtext("Mugshots", side = 3, line = 1, outer = TRUE)
 
 # finish
